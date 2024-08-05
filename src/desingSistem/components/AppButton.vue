5a58997e5ef267405b2d0db3a12df694979d87d1
Derian Rosado <derian.rosado@alegra.com>
2024-08-05 13:36:51 -0600

    chore: Update AppButton, AppField, AppForm, and AppLine components

--- src/desingSistem/components/AppButton.vue
+++ src/desingSistem/components/AppButton.vue
@@ -1,6 +1,40 @@
 <template>
-  <button type="submit">
-    <slot />
-  </button>
+  <div class="d-button">
+    <button
+      :class="['p-2 my-2 w-full rounded-lg text-white text-center', type]"
+      type="submit"
+    >
+      <slot />
+    </button>
+  </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { defineProps, withDefaults } from "vue";
+
+withDefaults(
+  defineProps<{
+    type: "primary" | "secondary" | "danger";
+  }>(),
+  {
+    type: "primary",
+  }
+);
+</script>
+
+<style scoped>
+.d-button {
+  @apply w-full;
+}
+
+.d-button .primary {
+  @apply bg-[#FF797B];
+}
+
+.d-button .secondary {
+  @apply bg-[#ad5153];
+}
+
+.d-button .danger {
+  @apply bg-[#ff4d50];
+}
+</style>
