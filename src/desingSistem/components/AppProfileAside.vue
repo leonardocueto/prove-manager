7ced306deccc97be755ea0588804b92bcc2ceb47
Derian Rosado <derian.rosado@alegra.com>
2024-08-12 13:19:37 -0600

    refactor: Improve AppProfileAside component with transition and styling updates

--- src/desingSistem/components/AppProfileAside.vue
+++ src/desingSistem/components/AppProfileAside.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flex gap-3 items-center">
-    <div class="bg-purple-300 rounded-full size-10"></div>
-    <template v-if="hide">
-      <div class="flex flex-col text-sm">
+    <div class="bg-purple-300 rounded-full min-w-10 min-h-10"></div>
+    <transition>
+      <div v-if="hide" class="flex flex-col text-sm truncate">
         <p class="font-bold">{{ name }}</p>
         <p class="text-gray-400">{{ job }}</p>
       </div>
-    </template>
+    </transition>
   </div>
 </template>
 <script lang="ts" setup>
@@ -18,3 +18,17 @@ defineProps<{
   hide: boolean;
 }>();
 </script>
+
+<style scoped>
+.v-enter-active {
+  transition: opacity 1s ease;
+}
+.v-leave-active {
+  transition: opacity 0s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
